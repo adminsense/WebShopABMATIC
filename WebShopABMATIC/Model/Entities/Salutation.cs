3be8d7445af87b4b9e635f91f45eb2ca9c0c3add
Muller <muller@adminsense.be>
2026-05-31 09:38:07 -0300

    new entities reviewed

---
+++ WebShopABMATIC/Model/Entities/Salutation.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Crm].[Salutations] (legacy: [Crm].[Aanspreking]).</summary>
+public class Salutation
+{
+    public int Id { get; set; }
+    public string SalutationText { get; set; }
+    public bool IsMale { get; set; }
+    public bool IsFemale { get; set; }
+    public int LanguageId { get; set; }
+}
+
---
+++ WebShopABMATIC/Model/Entities/ServiceRate.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Products].[ServiceRates] (legacy: [Products].[PrestatieTarief]).</summary>
+public class ServiceRate
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Rate { get; set; }
+}
+
