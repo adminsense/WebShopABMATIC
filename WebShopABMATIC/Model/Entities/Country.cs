0610b7d9bb3e1dada68bfa9f14dd68b4c9db3c2f
Muller <muller@adminsense.be>
2026-05-31 14:29:58 -0300

    identity

---
+++ WebShopABMATIC/Model/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Crm].[Country] (legacy: [Crm].[Country]).</summary>
+public class Country
+{
+    public int Id { get; set; }
+    public bool? IsEu { get; set; }
+    public string IsoCode { get; set; }
+    public string? Name { get; set; }
+}
+
