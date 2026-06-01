fa31ae5748d43d8d5414d0549332c866a1e545bb
Muller <muller@adminsense.be>
2026-06-01 14:37:15 -0300

    more entities and services

---
+++ Model/Entities/AutoNumbering.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Settings].[AutoNumberings] (legacy: [Instellingen].[AutoNummering]).</summary>
+public class AutoNumbering
+{
+    public int Id { get; set; }
+    public int Number { get; set; }
+    public string Description { get; set; }
+    public string Prefix { get; set; }
+    public string Tag { get; set; }
+}
+
