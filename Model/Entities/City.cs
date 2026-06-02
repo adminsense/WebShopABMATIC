d58d2e91e20c35bc688b8aa9055561fbb13dc4ee
Muller <muller@adminsense.be>
2026-06-02 12:51:51 -0300

    log entities

---
+++ Model/Entities/City.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Crm].[City] (legacy: [Crm].[City]).</summary>
+public class City
+{
+    public int CityId { get; set; }
+    public string CityName { get; set; }
+    public string PostalCode { get; set; }
+    public string CountryName { get; set; }
+    public string? CountryIsoCode { get; set; }
+    public int? CountryId { get; set; }
+}
+
