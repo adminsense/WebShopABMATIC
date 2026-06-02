802181389d1d1efbb74ef6b3ece2b385f6089057
Muller <muller@adminsense.be>
2026-06-02 14:32:51 -0300

    context factory

---
+++ Application/Auth/AppPolicies.cs
@@ -0,0 +1,8 @@
+namespace WebShopABMATIC.Application.Auth;
+
+public static class AppPolicies
+{
+    public const string AdminOnly = "AdminOnly";
+    public const string AdminOrManager = "AdminOrManager";
+    public const string CustomerOnly = "CustomerOnly";
+}
