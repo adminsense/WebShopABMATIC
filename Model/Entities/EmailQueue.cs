530905db9096a506b9c73c18e11cdb073ec3c9be
Muller <muller@adminsense.be>
2026-06-03 18:21:10 -0300

    queues jobcodes

---
+++ Model/Entities/EmailQueue.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Emails].[EmailQueues] (legacy: [Emails].[EmailQueue]).</summary>
+public class EmailQueue
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
