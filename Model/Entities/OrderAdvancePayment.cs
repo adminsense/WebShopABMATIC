beffb0c859f585092b1d17a626cc2a9ecd7049a4
Muller <muller@adminsense.be>
2026-06-03 16:59:50 -0300

    jobs

---
+++ Model/Entities/OrderAdvancePayment.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Projects].[OrderAdvancePayments] (legacy: [Projecten].[DossierVoorschot]).</summary>
+public class OrderAdvancePayment
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public string Name { get; set; }
+    public decimal Percent { get; set; }
+    public bool IsFinalInvoice { get; set; }
+    public DateTime? InvoicedAt { get; set; }
+    public int SortOrder { get; set; }
+    public decimal? Amount { get; set; }
+    public string? AdvancePaymentVisibility { get; set; }
+}
+
