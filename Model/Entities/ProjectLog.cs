e646bd716cf1facc027aafba0b24baa45da16b70
Muller <muller@adminsense.be>
2026-06-05 11:15:08 -0300

    new entities

---
+++ Model/Entities/ProjectLog.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Projects].[ProjectLog] (legacy: [Projecten].[ProjectLog]).</summary>
+public class ProjectLog
+{
+    public int Id { get; set; }
+    public int ProjectId { get; set; }
+    public string ActionCode { get; set; }
+    public DateTime Date { get; set; }
+}
+
---
+++ Model/Entities/SickLeave.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System;
+
+namespace WebShopABMATIC.Data.Entities;
+
+/// <summary>Entity for [Users].[SickLeaves] (legacy: [Users].[Ziekte]).</summary>
+public class SickLeave
+{
+    public int Id { get; set; }
+    public DateTime FromAddress { get; set; }
+    public DateTime ValidTo { get; set; }
+    public long? SickNoteAzureFileId { get; set; }
+    public int UserId { get; set; }
+    public int? CalendarEntryId { get; set; }
+}
+
