3b1de13892fc2d6d5611ea030d3746c5d2ba3b0c
Muller <muller@adminsense.be>
2026-06-22 17:05:08 -0300

    fixes

--- Application/Ports/Inbound/ICustomerRegistrationPort.cs
+++
@@ -1,8 +0,0 @@
-using WebShopABMATIC.Application.Store.Registration;
-
-namespace WebShopABMATIC.Application.Ports;
-
-public interface ICustomerRegistrationPort
-{
-    Task<CustomerRegistrationResult> RegisterAsync(CustomerRegistrationRequest request, CancellationToken cancellationToken = default);
-}
--- Application/Stock/StockManualAdjustmentCommand.cs
+++
@@ -1,9 +0,0 @@
-namespace WebShopABMATIC.Application.Stock;
-
-public sealed class StockManualAdjustmentCommand
-{
-    public required int ProductId { get; init; }
-    public required int StockLocationId { get; init; }
-    public required decimal QuantityChange { get; init; }
-    public required string Reason { get; init; }
-}
