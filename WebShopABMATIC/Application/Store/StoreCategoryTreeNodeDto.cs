66a4ce107dda515cac48328600abb89e8018cb48
Muller <muller@adminsense.be>
2026-06-25 16:14:56 -0300

    fixes

---
+++ WebShopABMATIC/Application/Store/StoreCategoryTreeNodeDto.cs
@@ -0,0 +1,11 @@
+namespace WebShopABMATIC.Application.Store;
+
+public sealed class StoreCategoryTreeNodeDto
+{
+    public int Id { get; init; }
+    public int? ParentId { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public int Level { get; init; }
+    public int? ProductCount { get; init; }
+    public IReadOnlyList<StoreCategoryTreeNodeDto> Children { get; init; } = [];
+}
