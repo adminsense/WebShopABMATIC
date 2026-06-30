99160d3418d0bd8c0efd0c489226d912d77d5d7f
Muller <muller@adminsense.be>
2026-06-30 15:17:19 -0300

    fixes

--- WebShopABMATIC.Client/Components/Account/LegacyAuthenticationStateProvider.cs
+++ WebShopABMATIC.Client/Components/Account/LegacyAuthenticationStateProvider.cs
@@ -134,3 +134,11 @@ public sealed class LegacyAuthenticationStateProvider : RevalidatingServerAuthen
         _subscription.Dispose();
     }
 }
+
+internal sealed class UserInfo
+{
+    public string? UserId { get; set; }
+    public string? Name { get; set; }
+    public string? DisplayName { get; set; }
+    public string[] Roles { get; set; } = [];
+}
