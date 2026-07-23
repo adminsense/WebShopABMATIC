e9c40da84b9a46651d21dc1725ad6bf662ca7498
Muller <muller@adminsense.be>
2026-07-23 17:14:08 -0300

    catalog tests

---
+++ WebShopABMATIC.Tests/Bunit/Admin/UserGroupListTests.cs
@@ -0,0 +1,16 @@
+﻿using Bunit;
+using FluentAssertions;
+using WebShopABMATIC.Web.Components.Pages.Admin;
+
+namespace WebShopABMATIC.Tests.Bunit.Admin;
+
+public sealed class UserGroupListTests : AdminPageTestBase
+{
+    [Fact]
+    public void Renders_without_throw()
+    {
+        AsStaff();
+        var cut = RenderComponent<UserGroupList>();
+        cut.Markup.Should().NotBeNullOrWhiteSpace();
+    }
+}
