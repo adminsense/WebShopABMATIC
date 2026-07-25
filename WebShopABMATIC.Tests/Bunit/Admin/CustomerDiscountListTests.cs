884d5ae62ac6c9c5d00173d82d8e13b2bbd013ef
Muller <muller@adminsense.be>
2026-07-25 10:13:56 -0300

    integration tests

---
+++ WebShopABMATIC.Tests/Bunit/Admin/CustomerDiscountListTests.cs
@@ -0,0 +1,16 @@
+﻿using Bunit;
+using FluentAssertions;
+using WebShopABMATIC.Web.Components.Pages.Admin;
+
+namespace WebShopABMATIC.Tests.Bunit.Admin;
+
+public sealed class CustomerDiscountListTests : AdminPageTestBase
+{
+    [Fact]
+    public void Renders_without_throw()
+    {
+        AsStaff();
+        var cut = RenderComponent<CustomerDiscountList>();
+        cut.Markup.Should().NotBeNullOrWhiteSpace();
+    }
+}
---
+++ WebShopABMATIC.Tests/Bunit/Admin/HubTests.cs
@@ -0,0 +1,16 @@
+﻿using Bunit;
+using FluentAssertions;
+using WebShopABMATIC.Web.Components.Pages.Admin;
+
+namespace WebShopABMATIC.Tests.Bunit.Admin;
+
+public sealed class HubTests : AdminPageTestBase
+{
+    [Fact]
+    public void Renders_without_throw()
+    {
+        AsStaff();
+        var cut = RenderComponent<Hub>(p => p.Add(x => x.HubId, "catalog"));
+        cut.Markup.Should().NotBeNullOrWhiteSpace();
+    }
+}
